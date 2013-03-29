f295179633d9dcfd44eb09ad2c68a1afd2e4656f
Walter P Babic <wbabic@mac.com>
2013-03-29 08:46:21 -0600

    initial commit
    yeooman bower grunt
    angularjs d3
    modern html5 webapp

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('yeomanAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'd3',
+      'AngularJS',
+      'Karma',
+      'Twitter Bootstrap'
+    ];
+  });
